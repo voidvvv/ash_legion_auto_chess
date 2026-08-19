1138d52cc9fd8a09efcc2b2b72c9d1ec6bb1073f
Zhao Kaijie <19370986818@163.com>
2026-08-19 23:02:55 +0800

    chore: add libgdx project skeleton and design doc

---
+++ android/src/main/java/com/voidvvv/kz_auto_chess_n/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.voidvvv.kz_auto_chess_n.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.voidvvv.kz_auto_chess_n.Main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Main(), configuration);
+    }
+}
